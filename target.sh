07231dd5289d46132edcf75c9170721c659b1c98
AnGgIt86 <jarbull86@gmail.com>
2021-06-29 06:33:26 +0700

    change target build kernel NFS

--- target.sh
+++ target.sh
@@ -3,8 +3,9 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER= # Change with your own name or else.
-export KBUILD_BUILD_HOST= # Change with your own hostname.
+KERNEL_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/NFS_4.9-rosy # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=NeedForSpeed_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/proton # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=xiaomi # Change with your own name or else.
+export KBUILD_BUILD_HOST=nfs-projects # Change with your own hostname.
+export LOCALVERSION=
