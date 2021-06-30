1761da05080d25d15628abc73b50e04bba8fe346
AnGgIt86 <jarbull86@gmail.com>
2021-06-30 14:21:27 +0700

    Add compiller GCC

---
+++ check-GCC.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+YL="\033[1;33m"
+W="\033[37;1m"
+B="\033[0;36m"
+R="\033[31;1m"
+GL="\033[32;1m"
+P="\033[35;1m"
+
+# Copyright (C) 2021 a xyzprjkt property
+
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+echo -e "$P""=========================================="
+echo -e "$YL""            ~MEMBANGUN KERNEL~              "
+echo -e "$P""=========================================="
+echo -e "$GL""             _  __  ____  ____
+            / |/ / / __/ / __/
+     __    /    / / _/  _\ \    __
+    /_/   /_/|_/ /_/   /___/   /_/"
+echo -e "$YL""   ___  ___  ____     _________________
+  / _ \/ _ \/ __ \__ / / __/ ___/_  __/
+ / ___/ , _/ /_/ / // / _// /__  / /
+/_/  /_/|_|\____/\___/___/\___/ /_/"
+echo -e "$P""=========================================="
+echo -e "$YL""         AnGgIt86 NFS (OFFICIAL)"
+echo -e "$P""=========================================="
+echo -e "$B"BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo CLANG_ROOTDIR = ${GCC_ROOTDIR}
+echo KERNEL_ROOTDIR = ${GCC_ROOTDIR}
+echo -e "$P"==========================================
+echo -e "$W"
---
+++ compile-GCC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright (C) 2021 a xyzprjkt property
+
+# Warning !! Dont Change anything there without known reason.
+cd ${KERNEL_ROOTDIR}
+make -j$(nproc --all) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+make -j$(nproc --all) ARCH=arm64 O=out \
+	CC=${GCC_ROOTDIR}/bin/gcc \
+	CROSS_COMPILE=${GCC_ROOTDIR}/bin/aarch64-linux-gnu- \
+	
