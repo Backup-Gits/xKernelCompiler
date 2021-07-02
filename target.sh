944c306f020b8e006b9d97b8bd2863b9293f5c74
AnGgIt86 <jarbull86@gmail.com>
2021-07-02 15:47:03 +0700

    update tc using xRageChain CLANG 13

--- target.sh
+++ target.sh
@@ -5,7 +5,7 @@
 # Main
 KERNEL_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/NFS_4.9-rosy # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_DEFCONFIG=NeedForSpeed_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/proton # IMPORTANT! Put your clang directory here.
+CLANG_ROOTDIR=~/work/UbuntuSSH/UbuntuSSH/xRageChain # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=xiaomi # Change with your own name or else.
 export KBUILD_BUILD_HOST=nfs-projects # Change with your own hostname.
 export LOCALVERSION=
